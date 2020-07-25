bb77e95dcce9f9c70ffe621b3dade3dc1d16a607
JaeseongPark <qkrwotjd1445@naver.com>
2020-07-25 18:01:19 +0900

    Sequelize DB Model 셋팅 완료

--- models/index.js
+++ models/index.js
@@ -11,24 +11,13 @@ db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
 // 테이블
-db.User = require('./user')(sequelize, Sequelize);
-db.Marketing = require('./marketing')(sequelize, Sequelize);
-db.Store = require('./store')(sequelize, Sequelize);
-db.Product = require('./product')(sequelize, Sequelize);
-db.Category = require('./category')(sequelize, Sequelize);
+db.Rollingpaper = require('./rollingpaper')(sequelize, Sequelize);
+db.RollingpaperContent = require('./rollingpaperContent')(sequelize, Sequelize);
 
 
 // 테이블간 관계 (Association)
-// 유저 : 마케팅 수신 동의 여부 = 1 : 1
-db.User.hasOne(db.Marketing); // User가 Marketing에 대한 정보를 가진다. 
-db.Marketing.belongsTo(db.User); // Marketing에 대한 정보는 User에 속해있다.
-
 // 가게 : 상품 = 1 : N
-db.Store.hasMany(db.Product);
-db.Product.belongsTo(db.Store);
-
-// 상품 : 카테고리 = M : N
-db.Product.belongsToMany(db.Category, { through: 'category_product' });
-db.Category.belongsToMany(db.Product, { through: 'category_product' });
+db.Rollingpaper.hasMany(db.RollingpaperContent);
+db.RollingpaperContent.belongsTo(db.Rollingpaper);
 
 module.exports = db;
