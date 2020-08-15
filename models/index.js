8988bf9170fe6fd7adfee21512e10937b0683553
JaeseongPark <qkrwotjd1445@naver.com>
2020-08-15 14:31:15 +0900

    스티커 생성, 조회, 삭제 라우팅 생성 완료

--- models/index.js
+++ models/index.js
@@ -13,11 +13,16 @@ db.sequelize = sequelize;
 // 테이블
 db.Rollingpaper = require('./rollingpaper')(sequelize, Sequelize);
 db.RollingpaperContent = require('./rollingpaperContent')(sequelize, Sequelize);
+db.Sticker = require('./sticker')(sequelize, Sequelize);
 
 
 // 테이블간 관계 (Association)
-// 가게 : 상품 = 1 : N
+// 롤링페이퍼 : 포스트들 = 1 : N
 db.Rollingpaper.hasMany(db.RollingpaperContent);
 db.RollingpaperContent.belongsTo(db.Rollingpaper);
+
+// 롤링페이퍼 : 스티커들 = 1 : N
+db.Rollingpaper.hasMany(db.Sticker);
+db.Sticker.belongsTo(db.Rollingpaper);
 
 module.exports = db;
--- routes/api/v1/index.js
+++ routes/api/v1/index.js
@@ -2,5 +2,6 @@ var express = require('express');
 var router = express.Router();
 
 router.use('/rolling', require('./rolling'));
+router.use('/sticker', require('./sticker'));
 
 module.exports = router;
